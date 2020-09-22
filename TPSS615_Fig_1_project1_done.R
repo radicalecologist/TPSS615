6acc5938f830b5e3e6ee520a772fab978354d677
radicalecologist <71678927+radicalecologist@users.noreply.github.com>
2020-09-21 15:41:01 -1000

    Add files via upload

--- TPSS615_Fig_1_project1_done.R
+++ TPSS615_Fig_1_project1_done.R
@@ -1,6 +1,7 @@
+## This is a recreation of figure 1B from Lynch & Connery 2003
 install.packages("tidyverse")
 library(tidyverse)
-data <- read.csv("Lynch_Eukar.csv")
+data <- read.csv("Lynch_Eukar.csv") #rename file 
 view(data)
 
 #changing axis names
@@ -11,28 +12,19 @@ names(data)[names(data) == "Number.of.Genes"]	= "gene.num"
 install.packages("scales")
 library(scales)
 
-
-#Gene Number portion
+#axes are not faithful to original
 ggplot(data= data) + 
-  geom_point(shape=1, aes(x=genome.size, y=log(gene.num)))+
-  xlim(0,1000)
   
-#NeU portion
-ggplot(data) +
-  geom_point(aes(x = log(genome.size), y=log(NeU))) +
-  theme_classic() +
-  geom_smooth(aes(x = log(genome.size), y=log(NeU)), method = "lm", se=FALSE, col = "black")
-
-  
-#combined, couldn't figure out how to do the y axis break
-ggplot(data= data) + 
-  geom_point(aes(x = log(genome.size), y = log(gene.num)), shape = 1) +
+  #this portion relates to the Gene Number section of the graph
+  geom_point(aes(x = log(genome.size), y = log(gene.num)), shape = 1) + # shape = 1 makes points hollow 
   geom_smooth(aes(x = log(genome.size), y=log(gene.num)), method = "loess", se=FALSE, col = "black")+
+  
+  #this portion relates to the NeU portion of the graph
   geom_point(aes(x=log(genome.size), y=log(NeU)), col="black")+
   geom_smooth(aes(x = log(genome.size), y=log(NeU)), method = "lm", se=FALSE, col = "black",  linetype = "longdash")+
-  theme_classic()+
-  xlab("Genome Size (Mb)")+
-  ylab(" ")+
-  annotate("text", label = "NeU", x = 6, y = -2, size = 6, colour = "black")  +  #annotates plot
-  annotate("text", label = "Gene Number", x = 6, y = 7, size = 6, colour = "black")   #annotates plot
+  theme_classic()+ #makes background white
+  xlab("Genome Size (Mb)")+ #label for x axis
+  ylab(" ")+ #label for y axis, no label, so just put a space
+  annotate("text", label = "NeU", x = 6, y = -2, size = 6, colour = "black")  +  #annotate, places "NeU" near points
+  annotate("text", label = "Gene Number", x = 6, y = 7, size = 6, colour = "black")   #annotate, places "Gene Number" near points
 
